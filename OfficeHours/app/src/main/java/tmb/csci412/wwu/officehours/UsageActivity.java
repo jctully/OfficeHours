9a07156402a93f62472643b502a1e80e08bb1bfa
farriem <farriem@wwu.edu>
2019-05-24 15:17:11 -0700

    Usage Guide implemented instead of settings

---
+++ OfficeHours/app/src/main/java/tmb/csci412/wwu/officehours/UsageActivity.java
@@ -0,0 +1,16 @@
+package tmb.csci412.wwu.officehours;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by farriem on 5/24/19.
+ */
+
+public class UsageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usage);
+    }
+}
